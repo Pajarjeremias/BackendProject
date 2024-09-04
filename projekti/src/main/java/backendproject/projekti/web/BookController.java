6f6a8845247815f43a4108bd4044333b36368388
Pajarjeremias <kormyy@hotmail.com>
2024-09-04 22:10:53 +0300

    4.9 tehdyt tehtävät

--- projekti/src/main/java/backendproject/projekti/web/BookController.java
+++ projekti/src/main/java/backendproject/projekti/web/BookController.java
@@ -3,8 +3,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import backendproject.projekti.domain.Book;
 import backendproject.projekti.domain.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+
+
 
 @Controller
 
@@ -16,5 +27,32 @@ public class BookController {
     public String bookList(Model model){
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+    @GetMapping("/mainpage")
+    public String ShowMeTheMainPage(Model model) {
+        return "main";
+    }
+    
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+      @PostMapping("/savebook")
+    public String saveBook(@ModelAttribute Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    } 
+    @RequestMapping("/editBook/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("editBook", repository.findById(id));
+        return "editBook";
+    }
+    
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
     }
 }
